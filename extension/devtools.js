317d68a2239375927a0d8a43812ab685d573bd66
Timo Reymann <mail@timo-reymann.de>
2022-09-25 17:17:27 +0200

    feat: Set icon based on dev tools theme for firefox

--- extension/devtools.js
+++ extension/devtools.js
@@ -1,7 +1,8 @@
 const browserAPI = chrome || browser
+const isDarkTheme = browserAPI.devtools.panels.themeName === "dark"
 
 browserAPI.devtools.panels.create("Custom Dev-Tools",
-    "panel/settings-gears.png",
+    `panel/settings-gears-${isDarkTheme ? "gray" : "black"}.png`,
     "panel/panel.html",
      (panel) => {
     }
