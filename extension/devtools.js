1e1ab7f151883e8024a14e18881c79ef4cde7e04
Timo Reymann <mail@timo-reymann.de>
2022-09-25 18:19:59 +0200

    chore: Move icons to subfolder

--- extension/devtools.js
+++ extension/devtools.js
@@ -2,7 +2,7 @@ const browserAPI = chrome || browser
 const isDarkTheme = browserAPI.devtools.panels.themeName === "dark"
 
 browserAPI.devtools.panels.create("Custom Dev-Tools",
-    `panel/settings-gears-${isDarkTheme ? "gray" : "black"}.png`,
+    `panel/icons/settings-gears-${isDarkTheme ? "gray" : "black"}.png`,
     "panel/panel.html",
      (panel) => {
     }
